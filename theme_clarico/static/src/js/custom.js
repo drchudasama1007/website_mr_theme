ed2bcc131b3121a77f990b97f7187c05f6ffa321
Sophia L <sophia.opensys@gmail.com>
2020-09-15 22:05:05 +0530

    [ADD]code added

--- theme_clarico/static/src/js/custom.js
+++ theme_clarico/static/src/js/custom.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-      items:4,
+      items:3,
       autoplay:false,
       margin:30,
       loop:true,
       nav:true,
-      navText:["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>" ]
+      navText:["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>" ]
   });
 });
